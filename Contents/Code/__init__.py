6ba8cf8719243c69ce57570ca47a44dcaf3807f9
Mike <mike.d.meier@gmail.com>
2012-11-12 10:27:34 -0600

    finished major overhaul

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,22 +1,22 @@
-
+ParseStories = SharedCodeService.ParseStories.ParseStories
 
 NPR_ROOT       = 'http://api.npr.org'
 API_KEY        = 'MDAyNTU3MTA2MDEyMjk2NTE1MzEwN2U0MQ001'
 LIST_URL       = NPR_ROOT + '/list?apiKey=' + API_KEY
-QUERY_URL      = NPR_ROOT + '/query?numResults=20&apiKey=' + API_KEY
+QUERY_URL      = NPR_ROOT + '/query?id=%s&numResults=20&requiredAssets=audio&apiKey=' + API_KEY
 SEARCH_URL     = NPR_ROOT + '/query?startNum=0&sort=dateDesc&output=NPRML&numResults=20&apiKey=' + API_KEY
-CACHE_INTERVAL = 600
 
-dirs = [ ['Topics', '3002'], 
-				 ['Music Genres', '3018'], 
-				 ['Programs' , '3004'],
-				 ['Bios' , '3007'],
-				 ['Music Artists' , 'music'],
-				 ['Columns' , '3003'],
-				 ['Series' , '3006'] ]
-				 
-musicDirs = [ ['Recent Artists', '3008'],
-							['All Artists', '3009'] ]
+dirs = [
+	['Topics', '3002'], 
+	['Music Genres', '3018'], 
+	['Programs' , '3004'],
+	['Bios' , '3007'],
+	['Music Artists' , 'music'],
+	['Columns' , '3003'],
+	['Series' , '3006']
+	]
+
+musicDirs = [ ['Recent Artists', '3008'], ['All Artists', '3009'] ]
 
 ####################################################################################################
 
@@ -31,12 +31,11 @@ def MainMenu():
 	oc = ObjectContainer()
 	for name, value in dirs:
 		if value == 'music':
-            oc.add(DirectoryObject(key=Callback(MusicMenu), title=name))
+			oc.add(DirectoryObject(key=Callback(MusicMenu), title=name))
 		else:
-			dir.Append(Function(DirectoryItem(SectionMenu, title=name), id=value))
-            oc.add(DirectoryObject(key=Callback(SectionMenu, id=value, name=name), title=name))
-    ###TODO - implement Search Service ###
-	#dir.Append(Function(InputDirectoryItem(Search, title="Search...", prompt="Search NPR", thumb=R("icon-search.png"))))
+		        oc.add(DirectoryObject(key=Callback(SectionMenu, id=value, name=name), title=name))
+	oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.npr", title="Search", summary="Search NPR for...", prompt="Search for...",
+		thumb=R("search.png")))
 	return oc
 
 ####################################################################################################
@@ -44,7 +43,7 @@ def MainMenu():
 def MusicMenu():
 	oc = ObjectContainer(title2="Music Artists")
 	for name, value in musicDirs:
-        oc.add(DirectoryObject(key=Callback(SectionMenu, id=value, name=name), title=name))
+		oc.add(DirectoryObject(key=Callback(SectionMenu, id=value, name=name), title=name))
 	return oc
 	
 ####################################################################################################
@@ -52,54 +51,18 @@ def MusicMenu():
 def SectionMenu(id, name):
 	oc = ObjectContainer(title2=name)
 	maxNumToReturn = 200
-	for item in XML.ElementFromURL(LIST_URL + '&id=' + id, cacheTime=CACHE_INTERVAL).xpath('//item'):
-        oc.add(DirectoryObject(key=Callback(StoryMenu, id=item.get('id'), name=item.get('title')),
-            title=item.get('title'), summary=item.get('additionalInfo')))
+	for item in XML.ElementFromURL(LIST_URL + '&id=' + id).xpath('//item'):
+		item_id = item.get('id')
+		item_url = QUERY_URL % item_id
+		item_title = item.xpath('./title')[0].text
+		try:
+			item_summary = item.xpath('./additionalInfo')[0].text
+		except:
+			item_summary = ''
+		oc.add(DirectoryObject(key=Callback(ParseStories, url=item_url, name=item_title),
+			title=item_title, summary=item_summary))
 		if id == '3008':
 			maxNumToReturn = maxNumToReturn - 1
 			if maxNumToReturn <= 0: 
 				break
 	return oc
-
-####################################################################################################
-
-def StoryMenu(id, name):
-	oc = ObjectContainer(title2=name)
-	dir.Extend(ParseStories(QUERY_URL + '&id=' + id))
-	if len(dir) == 0:
-		return MessageContainer('No Audio', 'No audio files were found in this section.')
-	return dir
-
-####################################################################################################
-def GetThumb(url):
-  if url:
-    try:
-      data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
-      return DataObject(data, 'image/jpeg')
-    except:
-      pass
-
-  return Redirect(R('icon-default.jpg'))
-
-####################################################################################################
-# I think this shouldn't be necessary at all #
-def S(item, attr): 
-    try:
-		return item.find(attr).text.replace('<em>','').replace('</em>','').replace('&mdash;','-')
-	except:
-		return ''
-
-####################################################################################################
-###TODO - implement Search Service ###
-#def Search(sender, query):
-#    dir = MediaContainer(viewGroup='Details', title2="Search: " + query)
-#	url = SEARCH_URL + '&searchTerm=' + query.replace(' ', '%20')
-#	dir.Extend(ParseStories(url))
-#	return dir
-
-####################################################################################################
-# This will likely be handled by the new URL Service #
-def PlayMusic(url):
-	target = HTTP.Request(url, cacheTime=CACHE_INTERVAL).content.split('\n')[0]
-	target = target.split('<', 1)[0]
-	return Redirect(target)
